c925fbf4c23c39c9cf5e7f2a27ddc598ccdf8429
ics-ikeda <ikeda@ics-web.jp>
2018-03-30 15:50:45 +0900

    サービスワーカーにfetchを追加

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,8 +1,16 @@
 // 何もしない、PWAのために置いているだけ
-self.addEventListener('install', function(event) {
-  console.log('Service Worker installing.');
+
+// service-worker.js
+self.addEventListener('install', function (e) {
+  console.log('[ServiceWorker] Install');
 });
 
-self.addEventListener('activate', function(event) {
-  console.log('Service Worker activating.');
+self.addEventListener('activate', function (e) {
+  console.log('[ServiceWorker] Activate');
+});
+
+// 現状では、この処理を書かないとService Workerが有効と判定されないようです
+// DevToolで［Add to homescreen］を試すと「Site cannot be installed: the page does not work offline」と表示されます
+self.addEventListener('fetch', function (event) {
+  console.log('[ServiceWorker] Fetch');
 });
