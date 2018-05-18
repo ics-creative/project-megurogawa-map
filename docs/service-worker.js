afee15ebfb644d181becc3cc80aa650fb9e82cf0
ics-ikeda <ikeda@ics-web.jp>
2018-05-18 16:53:33 +0900

    サービスワーカーにキャッシュ機能を追加

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,8 +1,18 @@
-// 何もしない、PWAのために置いているだけ
+const CACHE_NAME = 'project-megurogawa-map-v1';
+const urlsToCache = [
+  "./main.js"
+];
 
 // service-worker.js
 self.addEventListener('install', function (e) {
   console.log('[ServiceWorker] Install');
+
+  // インストール処理
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+
 });
 
 self.addEventListener('activate', function (e) {
@@ -13,4 +23,46 @@ self.addEventListener('activate', function (e) {
 // DevToolで［Add to homescreen］を試すと「Site cannot be installed: the page does not work offline」と表示されます
 self.addEventListener('fetch', function (event) {
   console.log('[ServiceWorker] Fetch');
+
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+
+        // キャッシュがあったのでレスポンスを返す
+        if (response) {
+          console.log('step - 1 - 1');
+          console.table(response);
+          return response;
+        }
+
+
+        // 重要：リクエストを clone する。リクエストは Stream なので
+        // 一度しか処理できない。ここではキャッシュ用、fetch 用と2回
+        // 必要なので、リクエストは clone しないといけない
+        const fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          (response) => {
+
+            // レスポンスが正しいかをチェック
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // 重要：レスポンスを clone する。レスポンスは Stream で
+            // ブラウザ用とキャッシュ用の2回必要。なので clone して
+            // 2つの Stream があるようにする
+            const responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then((cache) => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+  );
+
 });
